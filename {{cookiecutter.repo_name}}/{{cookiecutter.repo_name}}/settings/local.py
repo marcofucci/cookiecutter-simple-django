68fd4bcf27870de18587c3cba9b40fdf4626b5e5
Marco Fucci <marcofucci@gmail.com>
2013-08-21 09:25:10 +0100

    fixed local settings

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/local.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/local.py
@@ -23,11 +23,11 @@ DATABASES = {
 if len(sys.argv) > 1 and 'test' in sys.argv[1]:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': '/tmp/thepresentco_test.db',                      # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/tmp/{{cookiecutter.repo_name}}_test.db',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
         }
     }

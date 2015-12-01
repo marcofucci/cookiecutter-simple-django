8925e0727081d7a2fd14e33ee3b209489f107aac
Angel Velásquez <angvp@archlinux.org>
2015-12-01 17:43:57 -0300

    Remove patterns from urls.py (Django 1.8+)

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'tt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
